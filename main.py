f07afaf595f8059ef7f6a1cd0c44b6289447ac00
Tony Cunsolo <t27302013@correo.unimet.edu.ve>
2022-05-13 13:54:13 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,159 @@
+import music
+from nturl2path import url2pathname
+from sqlite3 import Timestamp
+from turtle import title
+import discord
+from discord.ext import commands
+import datetime
+import os
+from dotenv import load_dotenv
+
+servers ={}
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+bot=commands.Bot(command_prefix="<<", description="esto es un bot prueba")
+intends = discord.Intents.all()
+cogs = [music]
+
+for i in range(len(cogs)):
+    cogs[i].setup(bot)
+
+@bot.command(name="ping")
+async def ping(ctx):
+    await ctx.send("pong")
+
+@bot.command(name="add")
+async def sum(ctx, num1: int,num2: int):
+    await ctx.send(num1 + num2)
+
+@bot.command(name="prueba")
+async def info(ctx):
+    embed =  discord.Embed(title=f"{ctx.guild.name}",description="asasasasas",
+    timestamp=datetime.datetime.utcnow(),color=discord.Color.blue())
+    embed.add_field(name="server created at",value=f"{ctx.guild.created_at}")
+    embed.add_field(name="server owner: ",value=f"{ctx.guild.owner}")
+    embed.add_field(name="Server region: ",value=f"{ctx.guild.region}")
+    embed.add_field(name="Server Id: ", value=f"{ctx.guild.id}")
+    embed.set_thumbnail(url="http://assets.stickpng.com/images/5848152fcef1014c0b5e4967.png")
+    await ctx.send(embed=embed)
+
+
+@bot.command(name="youtube")
+async def youtube(ctx,*,search):
+    await ctx.send(music.Search(search))
+
+@bot.command(name="blw")
+async def Black_List_Words(ctx):
+    message =""
+    embed =  discord.Embed(title="Bad Words",
+    timestamp=datetime.datetime.utcnow(),color=discord.Color.blue())
+    for index in range(10):
+        if(len(servers[ctx.guild.id]["black_list_words"])>index):
+            message += "{}. {}\n".format(index+1,servers[ctx.guild.id]["black_list_words"][index]["word"])
+    embed.add_field(name="List ",value=message)
+    await ctx.send(embed=embed)
+
+@bot.command(name="ablw")
+async def Add_Black_List_Words(ctx,*,word):
+    if ctx.author.guild_permissions.administrator:
+        word = word.replace(" ","")
+        lista = word.rsplit(",")
+        continua = True
+        for index, content in enumerate(lista):
+            if index == 0:
+                continue
+            else:
+                if not (content.endswith(">") and content.startswith("<#")):
+                    continua = False
+
+        if len(lista) == 1 and continua:
+            servers[ctx.message.guild.id]["black_list_words"].append({
+
+                "word": word,
+                "channels": ["all"]
+            })
+
+            await ctx.send("{} was added to the list!".format(word))
+        elif continua:
+            servers[ctx.message.guild.id]["black_list_words"].append({
+
+                "word": lista[0],
+                "channels": lista
+            })
+            servers[ctx.message.guild.id]["black_list_words"][-1]["channels"].pop(0)
+            temp =servers[ctx.message.guild.id]["black_list_words"][-1]["channels"][-1]
+            temp2 = temp[2:-1]
+            servers[ctx.message.guild.id]["black_list_words"][-1]["channels"][-1] = temp2
+
+            await ctx.send("{} was added to the list!".format(servers[ctx.message.guild.id]["black_list_words"][-1]["word"]))
+        else:
+            await ctx.send("You have to put the channels with the # symbol")
+    else:
+        await ctx.send("You don't have the administration role for this")
+
+@bot.command(name="blp")
+async def Black_List_People(ctx):
+    message =""
+    embed =  discord.Embed(title="Bad People",
+    timestamp=datetime.datetime.utcnow(),color=discord.Color.blue())
+    for index in range(10):
+        if(len(servers[ctx.guild.id]["black_list_people"])>index):
+            message += "{}. {}\n".format(index+1,servers[ctx.guild.id]["black_list_people"][index]["name"])
+    embed.add_field(name="List ",value=message)
+    await ctx.send(embed=embed)
+
+#Events
+@bot.event
+async def on_message(message):
+
+    if(message.author.bot==False):
+
+        guilt_id = message.guild.id
+        if not guilt_id in servers:
+            servers[guilt_id] = {
+
+                "name":message.guild.name,
+                "black_list_people":[],
+                "black_list_words":[]
+            }
+        
+        else:
+            one_bad_word = False
+            found = False
+            message_temp = message.content.upper()
+            for words in servers[guilt_id]["black_list_words"]:
+
+                if one_bad_word:
+                    break
+
+                for number_channels in words["channels"]:
+
+                    if (not message_temp.find(words["word"].upper()) ==-1) and (number_channels== "all" or  str(message.channel.id) == number_channels):
+                        await message.delete()
+                        await message.channel.send("you can't say that")
+                        one_bad_word = True
+                        for user in servers[guilt_id]["black_list_people"]:
+
+                            if user["name"] == message.author.name:
+
+                                user["reasons"].append(message.content)
+                                found = True
+                        
+                        if not found:
+
+                            servers[guilt_id]["black_list_people"].append({
+
+                                "name": message.author.name,
+                                "reasons":[message.content]
+                            })
+
+                        break
+    await bot.process_commands(message)
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(status=discord.Status.dnd,activity=discord.CustomActivity(name="nothing"))
+    print("I'm ready")
+
+bot.run(TOKEN)
---
+++ music.py
@@ -0,0 +1,251 @@
+from posixpath import join
+from unicodedata import name
+import discord 
+from discord.ext import commands
+import youtube_dl
+from urllib import parse,request
+import re
+import os
+import math
+import datetime
+from random import random
+from discord import FFmpegPCMAudio
+
+class music(commands.Cog): 
+    def __init__(self, client):
+        self.client = client
+        self.song_queue = {}
+        self.actual_song =""
+        self.actual_title = ""
+        self.loop = False
+        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+        self.YDL_OPTIONS = {'format': 'bestaudio'}
+
+    @commands.command() 
+    async def join(self,ctx): 
+        if ctx.author.voice is None:
+            await ctx.send("You're not in a voice channel!")
+        voice_channel = ctx.message.author.voice.channel 
+        if ctx.voice_client is None:
+            await voice_channel.connect()
+        else:
+            await ctx.voice_client.move_to(voice_channel)
+
+    @commands.command() 
+    async def disconnect(self,ctx):
+        await ctx.voice_client.disconnect()
+
+    @commands.command() 
+    async def Meme_clip(self,ctx):
+        await self.join(ctx)
+        vc = ctx.voice_client
+        if not vc.is_playing():
+            num_d = len(os.listdir("./assets/Music"))
+            random_num = (int) (random() * (num_d))
+            vc = ctx.voice_client
+            for index, file in enumerate(os.listdir("./assets/Music")):
+                if index == random_num:
+                    print(f"file: {file} , number: {index}")
+                    vc.play(discord.FFmpegPCMAudio(executable="C:/Path_FFmpeg/ffmpeg.exe", source="./assets/Music/"+file))
+        else:
+            await ctx.send("wait until the bot isn't playing songs")
+
+    @commands.command() 
+    async def Meme_clip(self,ctx, random_num):
+        await self.join(ctx)
+        vc = ctx.voice_client
+        if not vc.is_playing():
+            vc = ctx.voice_client
+            for index, file in enumerate(os.listdir("./assets/Music")):
+                if index == int(random_num):
+                    print(f"file: {file} , number: {index}")
+                    vc.play(discord.FFmpegPCMAudio(executable="C:/Path_FFmpeg/ffmpeg.exe", source="./assets/Music/"+file))
+        else:
+            await ctx.send("wait until the bot isn't playing songs")
+    
+    @commands.command() 
+    async def play(self,ctx,*, url:str):
+        await self.join(ctx)
+        vc = ctx.voice_client
+        if url[:32] != "https://www.youtube.com/watch?v=" or url[:17] != "https://youtu.be/":
+            trueurl = Search(url)
+            
+        else:
+            if url.count < 30:
+                trueurl = url[:43]
+            else:
+                 trueurl = url
+        if vc.is_playing():
+            await self.ListMusic(ctx,trueurl)
+        else:
+            with youtube_dl.YoutubeDL(self.YDL_OPTIONS) as ydl:
+                info = ydl.extract_info(url=trueurl, download=False)
+                id = info["id"]
+                url2 = info["formats"][0]['url']
+                titulo = info['title']
+                try: 
+                    autor = info['creator']
+                except:
+                    autor = "???"
+                ano = info['upload_date']
+                duration = info['duration']
+                view = "{:,}".format(info['view_count'])
+                try:
+                    like = "{:,}".format(info['like_count'])
+                except:
+                    like = "???"
+
+                source = await discord.FFmpegOpusAudio.from_probe(url2,**self.FFMPEG_OPTIONS)
+                self.actual_song = source
+                self.actual_title = titulo
+                vc.play(source,after=lambda x=None: self.PlayAfter(ctx,ctx.message.guild.id))
+                print(f"playing: {titulo}")
+
+                embed = discord.Embed(title=f"{titulo}",description="now playing...",
+                timestamp=datetime.datetime.utcnow(),color=discord.Color.blue())
+                embed.add_field(name="Created by: ",value=f"{autor}")
+                embed.add_field(name="Year: ",value=f"{Years_Convertion(ano)}")
+                embed.add_field(name="duration: ",value=f"{Number_Time(duration)}")
+                embed.add_field(name="Views: ",value=f"{view}")
+                embed.add_field(name="Likes: ",value=f"{like}")
+                embed.set_thumbnail(url=f"https://img.youtube.com/vi/{id}/default.jpg")
+                await ctx.send(embed=embed)
+    
+    async def ListMusic(self,ctx,url:str):
+        with youtube_dl.YoutubeDL(self.YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url=url, download=False)
+            id = info["id"]
+            url2 = info["formats"][0]['url']
+            titulo = info['title']
+            try: 
+                autor = info['creator']
+            except:
+                autor = "???"
+            ano = info['upload_date']
+            duration = info['duration']
+            view = "{:,}".format(info['view_count'])
+            try:
+                like = "{:,}".format(info['like_count'])
+            except:
+                like = "???"
+
+            source = await discord.FFmpegOpusAudio.from_probe(url2,**self.FFMPEG_OPTIONS)
+
+        dictionary = {"Id": id,"Source": source,"Url":url2,"Title": titulo,
+        "Autor":autor,"Year":ano,"Duration":duration,"View": view,"Like":like}
+
+        guilt_id = ctx.message.guild.id
+        if guilt_id in self.song_queue:
+            self.song_queue[guilt_id].append(dictionary)
+        else:
+            self.song_queue[guilt_id] = [dictionary]
+        
+        await ctx.send("added to the list!")
+
+        embed = discord.Embed(title=f"{titulo}",description="in queue...",
+        timestamp=datetime.datetime.utcnow(),color=discord.Color.blue())
+        embed.add_field(name="Created by: ",value=f"{autor}")
+        embed.add_field(name="Year: ",value=f"{Years_Convertion(ano)}")
+        embed.add_field(name="duration: ",value=f"{Number_Time(duration)}")
+        embed.add_field(name="Views: ",value=f"{view}")
+        embed.add_field(name="Likes: ",value=f"{like}")
+        embed.set_thumbnail(url=f"https://img.youtube.com/vi/{id}/default.jpg")
+        await ctx.send(embed=embed)
+            
+    def PlayAfter(self,ctx,discord_id):
+        if not self.loop and (not self.song_queue == {} and self.song_queue[discord_id] !=[]):
+            voice = ctx.guild.voice_client
+            data = self.song_queue[discord_id].pop(0)
+            id = data["Id"]
+            titulo = data["Title"]
+            self.actual_song= data["Source"]
+            self.actual_title = titulo
+            ctx.voice_client.play(data["Source"],after=lambda x=None:self.PlayAfter(ctx,ctx.message.guild.id))
+            print(f"playing: {titulo}")
+        
+        elif self.loop:
+            ctx.voice_client.play(self.actual_song,after=lambda x=None:self.PlayAfter(ctx,ctx.message.guild.id))
+            print(f"playing: {self.actual_title}")
+
+    @commands.command(name="queue")
+    async def Queue(self,ctx):
+        discord_id = ctx.message.guild.id
+        titulos = []
+        if not self.song_queue == {} and self.song_queue[discord_id] !=[]:
+            for x in range(10):
+                if len(self.song_queue[discord_id]) > x:
+                    titulos.append(self.song_queue[discord_id][x]["Title"])
+                if x == 0:
+                    id = self.song_queue[discord_id][x]["Id"]
+                    image =f"https://img.youtube.com/vi/{id}/default.jpg"
+            
+            embed = discord.Embed(title="List of songs",
+            timestamp=datetime.datetime.utcnow(),color=discord.Color.blue())
+            embed.add_field(name="Titles: ",value=List_Queue(titulos))
+            embed.set_thumbnail(url=image)
+            await ctx.send(embed=embed)
+
+    @commands.command(name="loop") 
+    async def Single_Loop(self,ctx):
+        self.loop = not self.loop
+        if not self.actual_song == "":
+            if self.loop:
+                await ctx.send("{} is looped!".format(self.actual_title))
+            else:
+                await ctx.send("{} is no more looped!".format(self.actual_title))
+        else:
+            if self.loop:
+                await ctx.send("next song is going to be looped!")
+            else:
+                await ctx.send("next song is not going to be looped!")
+
+    @commands.command() 
+    async def pause(self,ctx):
+        ctx.voice_client.pause()
+        await ctx.send("Pause")
+
+    @commands.command() 
+    async def resume(self,ctx):
+        ctx.voice_client.resume()
+        await ctx.send("resume")
+
+    @commands.command() 
+    async def skip(self,ctx):
+        ctx.voice_client.stop()
+        await ctx.send("skip")
+
+
+def setup(client):
+        client.add_cog(music(client))
+
+def Search(search):
+    query_string = parse.urlencode({"search_query": search}) #convierte lo que puso el usuario en codigo url
+    html_content = request.urlopen("http://www.youtube.com/results?"+query_string) #agarra la url y lo transforma en un documento html
+    search_results=re.findall('watch\?v=(.{11})',html_content.read().decode('utf-8'))# crea un array con los id de los viddeos
+    results = "https://www.youtube.com/watch?v="+search_results[0]
+    return results
+
+def Number_Time(num:int):
+    hours = 0
+    while num >= 3600:
+        hours +=1
+        num -=3600
+    min = num/60
+    sec = min - math.floor(min)
+    if hours == 0:
+        return f"{math.floor(min)}:{round(sec*60)}"
+    else:
+        return f"{hours}:{math.floor(min)}:{round(sec*60)}"
+    
+def Years_Convertion(message:str):
+    year = message[:4]
+    month = message[6:8]
+    day = message[4:6]
+    return f"{day}/{month}/{year}"
+
+def List_Queue(titles):
+    text =""
+    for index,element in enumerate(titles):
+        text += "{}. {}\n".format(index+1,element)
+    return text
+
